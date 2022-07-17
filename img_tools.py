2596217af42dc8298c134670f3cd07d641e92435
theio1 <theio666@gmail.com>
2022-07-17 21:24:00 +0300

    added pytorch fc networc and DCGAN prototype

--- img_tools.py
+++ img_tools.py
@@ -2,18 +2,18 @@
 # rgba png export with r=g=b=255 and alpha for opacity
 # input with 000 -> 255 255 255 255, x x x 0 -> 255... x
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps, ImageChops
+
 import numpy as np
 
 from pathlib import Path
 
 
-def text_on_img(filename='test.png', out_folder='', text="8", font_file=None, auto_adjast=True):
+def text_on_img(filename='test.png', out_folder='', text="8", font_file=None, auto_adjast=True, pic_dim=128):
     # path preparation
     out_path = out_folder / filename
 
     # create image, transparent
-    pic_dim = 256
     pic_size = (pic_dim, pic_dim)
     image = Image.new(mode="RGBA", size=pic_size, color=(255, 255, 255, 0))
     draw = ImageDraw.Draw(image)
@@ -24,7 +24,7 @@ def text_on_img(filename='test.png', out_folder='', text="8", font_file=None, au
     else:
         font_file = str(font_file)
 
-    size = 256
+    size = pic_dim
     font = ImageFont.truetype(font_file, size)
     max_area = 0.90
     if auto_adjast:
@@ -64,22 +64,62 @@ def text_on_img(filename='test.png', out_folder='', text="8", font_file=None, au
     # os.system(str(out_path))
 
 
-def augmentation(img_arr, scale_arr=None, translation_arr=None, rotate_arr=None):
+def img_to_nparr(img, ):
+    # not needed for now
+    pass
+
+def augmentation(img_arr, scale_arr=None, translation_arr=None, rotate_arr=None, mode="np"):
+    img_size = img_arr[0].size[0]
+
     if scale_arr is None:
-        scale_arr = [0.85, 0.9, 0.95, 1]
+        scale_arr = [0.87, 0.9, 0.93, 0.96, 0.97, 1]
     if translation_arr is None:
-        translation_arr = list(range(-9, 10, 3))
+        # percentages!
+        translation_arr = list(range(-4, 5, 2))
+        translation_arr = [round(img_size*i/100) for i in translation_arr]
     if rotate_arr is None:
         rotate_arr = list(range(-6, 7))
+
     # full augmentation size
     total_samples = len(img_arr) * len(scale_arr) * len(translation_arr) * len(translation_arr) * len(rotate_arr)
     size_input = img_arr[0].size
-    ans_shape = (total_samples, size_input[0], size_input[1])
-    ans = np.zeros(ans_shape, dtype=np.single)
 
+    if mode == "np":
+        ans_shape = (total_samples, size_input[0], size_input[1])
+        ans = np.zeros(ans_shape, dtype=np.single)
+    elif mode == "img":
+        ans = [None] * total_samples
+
+
+    iter_pic = 0
     for img in img_arr:
         for angle in rotate_arr:
-            tmp_pic = img.rotate(angle, resample=Image.BILINEAR)
+            img_rotated = img.rotate(angle, resample=Image.BILINEAR)
+            for scale in scale_arr:
+                new_size = int(img_size * scale) // 2 * 2
+                img_scaled_small = ImageOps.pad(img_rotated, size=(new_size, new_size))
+                border = (img_size - img_scaled_small.size[0]) // 2
+                img_scaled = ImageOps.expand(img_scaled_small, border=border)
+                for offset_x in translation_arr:
+                    for offset_y in translation_arr:
+                        img_translated = ImageChops.offset(img_scaled, xoffset=offset_x, yoffset=offset_y)
+
+                        # this is final picture, not turn in into numpy arr and write to ans
+
+
+                        # flip to fix y-inversion
+                        # [:, :, 3] picks only alpha channel
+                        # we only need black and its opacity
+                        if mode == "np":
+                            img_np = np.asarray(img_translated)
+                            img_np_float = np.array((np.flip(img_np[:, :, 3], axis=0)) / 255)
+                            ans[iter_pic] = img_np_float
+
+                        elif mode == "img":
+                            r, g, b, a = img_translated.split()
+                            ans[iter_pic] = a
+                        iter_pic += 1
+
     return ans
 
 
